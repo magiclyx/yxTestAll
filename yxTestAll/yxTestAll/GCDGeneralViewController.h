e1ab2f672ea2d257c2eeb2915c26dcb6e5d3a951
Yuxi Liu <liu.yuxi.canaan@gmail.com>
2014-07-24 19:51:10 +0800

    添加了logViewController, 用于控制台窗口
    添加了部分GCD的代码

---
+++ yxTestAll/yxTestAll/GCDGeneralViewController.h
@@ -0,0 +1,13 @@
+//
+//  GCDGeneralViewController.h
+//  yxTestAll
+//
+//  Created by Yuxi Liu on 7/24/14.
+//  Copyright (c) 2014 Yuxi Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GCDGeneralViewController : UIViewController
+
+@end
---
+++ yxTestAll/yxTestAll/logViewController.h
@@ -0,0 +1,13 @@
+//
+//  logViewController.h
+//  yxTestAll
+//
+//  Created by Yuxi Liu on 7/24/14.
+//  Copyright (c) 2014 Yuxi Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface logViewController : UIViewController
+
+@end
